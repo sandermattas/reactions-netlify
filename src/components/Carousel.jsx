e5307b19b0273bf374987f9e75873e0d5aa36e45
Sander <sander.mattas@gmail.com>
2022-05-01 13:01:38 +0300

    Way to increase number of cats users see

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,12 +4,13 @@ import "../styles/Carouselcontainer.css";
 
 function Carousel(props) {
   const [current, setCurrent] = useState(0);
-  const { pictureURLs, catAmount } = props;
+  const { pictureURLs, catAmount, increaseCatAmount } = props;
 
   const catArray = new Array(catAmount);
 
   const clickNext = () => {
     setCurrent(current === catArray.length - 1 ? 0 : current + 1);
+    increaseCatAmount();
   };
   const clickPrevious = () => {
     setCurrent(current === 0 ? catArray.length - 1 : current - 1);
@@ -19,7 +20,11 @@ function Carousel(props) {
     <div className="Carouselcontainer">
       <button onClick={clickPrevious}>⇦</button>
       {pictureURLs.map((element, index) => (
-        <Caturday key={element} url={element.url} active={index === current} />
+        <Caturday
+          key={element.id}
+          url={element.url}
+          active={index === current}
+        />
       ))}
       <button onClick={clickNext}>⇨</button>
     </div>
--- src/components/CatCarouselPage.jsx
+++ src/components/CatCarouselPage.jsx
@@ -1,24 +1,35 @@
 import { useState, useEffect } from "react";
 import Carousel from "./Carousel";
 
+const getCats = (quantity, setCatResponse) => {
+  console.log("qty", quantity);
+  fetch(`https://api.thecatapi.com/v1/images/search?limit=${quantity}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setCatResponse(data);
+    });
+};
+
 function CatCarouselPage() {
   const [catResponse, setCatResponse] = useState([]);
-
-  const amount = 4;
+  const [amount, setAmount] = useState(2);
 
   useEffect(() => {
-    fetch(`https://api.thecatapi.com/v1/images/search?limit=${amount}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setCatResponse(data);
-      });
+    getCats(amount, setCatResponse);
   }, []);
 
   return (
     <div>
-      <Carousel pictureURLs={catResponse} catAmount={amount} />
+      <Carousel
+        pictureURLs={catResponse}
+        catAmount={amount}
+        increaseCatAmount={() => {
+          getCats(amount + 1, setCatResponse);
+          setAmount(amount + 1);
+        }}
+      />
     </div>
   );
 }
